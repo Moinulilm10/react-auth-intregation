e2335ba3ce4a799a75b019355402417f32f0e836
Moinul Islam <moinulilm10@gmail.com>
2021-10-17 21:40:56 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLvfNxtIaugBrxYvEb9Evoa4GIGN-ueXU",
+    authDomain: "react-auth-intregation-84d3b.firebaseapp.com",
+    projectId: "react-auth-intregation-84d3b",
+    storageBucket: "react-auth-intregation-84d3b.appspot.com",
+    messagingSenderId: "1078355585646",
+    appId: "1:1078355585646:web:a825a9c3b1131b5ea4547f"
+};
+
+export default firebaseConfig;
---
+++ src/components/Placeorder/Placeorder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Placeorder = () => {
+    return (
+        <div>
+            <h2>This is place order</h2>
+            <p>place your order</p>
+        </div>
+    );
+};
+
+export default Placeorder;
